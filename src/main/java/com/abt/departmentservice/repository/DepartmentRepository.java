70434dba9970f762b677e3a47189025dcbd0ae10
nmwamlima <mwamlimantonsite@gmail.com>
2021-01-20 08:57:34 +0300

    initial commit

---
+++ src/main/java/com/abt/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.abt.departmentservice.repository;
+
+import com.abt.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
---
+++ src/main/java/com/abt/departmentservice/service/DepartmentService.java
@@ -0,0 +1,12 @@
+package com.abt.departmentservice.service;
+
+import com.abt.departmentservice.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+}
